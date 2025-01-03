bfcd46af558e1f27fc932c808b0cce83b603ab02
Erick Chavez <erickchavezescobar@gmail.com>
2025-01-02 23:50:42 -0600

    TESTING

---
+++ script.js
@@ -0,0 +1,7 @@
+// const formEl = document.querySelector('.form')
+// formEl.addEventListener('mouseenter', () => {
+//   //   formEl.style.backgroundColor = 'yellow'
+//   formEl.classList.add('form_hover')
+// })
+
+// VIDEO 35 - MINUTE 19
